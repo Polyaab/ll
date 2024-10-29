dc57d9d914ea1e30fb644913ce5476ad482a77c7
Polyaab <147725191+Polyaab@users.noreply.github.com>
2024-10-29 19:39:28 +0800

    Create test_even_odd.py

---
+++ test_even_odd.py
@@ -0,0 +1,17 @@
+   import unittest
+   from even_odd import is_even
+
+   class TestEvenOdd(unittest.TestCase):
+
+       def test_even(self):
+           self.assertTrue(is_even(2))
+           self.assertTrue(is_even(0))
+           self.assertTrue(is_even(-4))
+
+       def test_odd(self):
+           self.assertFalse(is_even(3))
+           self.assertFalse(is_even(-1))
+
+   if __name__ == '__main__':
+       unittest.main()
+   
